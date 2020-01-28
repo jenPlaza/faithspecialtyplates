71b563bebcc556ad19b907a38b255485c70a7aa8
jenPlaza <39809852+jenPlaza@users.noreply.github.com>
2020-01-27 22:57:13 -0500

    completed js for shopping cart process

--- faithSpecialtyPlates/js/main.js
+++ faithSpecialtyPlates/js/main.js
@@ -1,42 +1,55 @@
-let imageArray = [
-    { ImageSrc: "crsPlate.png", ImageCaption: "A Catholic Foundation"},
-    { ImageSrc: "obiPlate.png", ImageCaption: "A Christian Foundation"},
-    { ImageSrc: "mazonPlate.png", ImageCaption: "A Jewish Foundation"},
-    { ImageSrc: "mercyUSAPlate.png", ImageCaption: "An Islamic Foundation"},
-	{ ImageSrc: "tzuPlate.png", ImageCaption: "A Buddhist Foundation"},
-	{ ImageSrc: "sewaPlate.png", ImageCaption: "A Hindu Foundation"},
-    { ImageSrc: "fspPlate.png", ImageCaption: "Faith Specialty Plates"}
-];
+// JavaScript Document
+window.addEventListener("load", function(){
+	
+	//instantiate singleton
+	let main = Main.getInstance();
+});
 
-let imageIndex = 0;
 
-let text = "<h2><font color=brown size=4>Featured Specialty Plates</font></h2>";
+class Main{
+	constructor(){
+		this.getForm();
+	}
+	
+	getForm(){
+		var cartButtons = document.querySelectorAll('.button');
+				//testing
+				//console.log(cartButtons.length);
+			
+		//loop through all task buttons
+		for (var i = 0; i < cartButtons.length; i++) {
+		cartButtons[i].addEventListener('click', function() {
+	
+				//testing
+    			console.log("btn id:", this.id);
 
-text += '<figure>';
-text += '<a href="/"><img id="mainImage" src="images/crsPlate.png" alt="Faith Specialty Plate" width="414" height="275"/></a>';
-text += '<figcaption>A Catholic Foundation</figcaption>';
-text += '</figure>';
-
-//Previous button
-var leftArrow = document.querySelector('#prev')[0];
-document.body.appendChild(leftArrow);
-leftArrow.addEventListener("click", prev);
-
-function prev() {
-    imageIndex--;
-    document.getElementById("mainImage").src = imageArray[imageIndex].ImageSrc;
-    document.getElementsByTagName("figcaption")[0].innerHTML = imageArray[imageIndex].ImageCaption;
+			//condition for each button
+			if (this.id == 'checkBtnNoRenew'){
+			document.querySelector("#noRenewal").style.display = "block";
+			document.querySelector("#renewal").style.display = "none";
+			}
+			else if(this.id == 'checkBtnRenew'){
+			document.querySelector("#renewal").style.display = "block";
+			document.querySelector("#noRenewal").style.display = "none";
+			}
+			else if(this.id == 'checkBtnRenewal'){
+				document.querySelector("#thankyou").style.display = "block";
+			}
+			else if(this.id == 'checkBtnNoRenewal'){
+				document.querySelector("#thankyou").style.display = "block";
+			}
+  			});
+		}
+}
+	
+	static getInstance(){
+		if(!Main._instance){
+			Main._instance = new Main(); 
+			return Main._instance;
+		}
+		else{
+			throw SyntaxError('Cannot create a second Course instance');
+			}
+		}
 }
 
-//Next button
-var rightArrow = document.querySelector('#next')[0];
-document.body.appendChild(rightArrow);
-rightArrow.addEventListener("click", next);
-
-function next() {
-    imageIndex++;
-    document.getElementById("mainImage").src = imageArray[imageIndex].ImageSrc;
-    document.getElementsByTagName("H3")[0].innerHTML = imageArray[imageIndex].ImageCaption;
-}
-
-document.getElementById("fsp").innerHTML = text;
