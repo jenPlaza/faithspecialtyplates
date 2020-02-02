9a7c0dc5e4648517b9a9503f993bb0977b6d8914
jenPlaza <39809852+jenPlaza@users.noreply.github.com>
2020-02-02 04:33:23 -0500

    Added arithmatic functionality to cart order

--- js/main.js
+++ js/main.js
@@ -1,1 +1,53 @@
+// JavaScript Document
+window.addEventListener("load", function(){
+	
+	//instantiate singleton
+	let main = Main.getInstance();
+});
 
+	class Main{
+	constructor(){
+		this.getNoRenewTotal();
+		this.getRenewTotal();
+	}
+	
+	getNoRenewTotal(){
+		var price = document.getElementsByName('price');
+        var quantity = document.getElementsByName('num');
+		var tot = 0;
+		
+			quantity[0].addEventListener("keyup", function() {
+
+            if(price[0].value != "" && quantity[0].value != "")
+			{
+             tot += parseInt(quantity[0].value) * parseInt(price[0].value);
+            document.querySelector("#total").innerHTML = tot;
+			}
+	});	
+}
+
+getRenewTotal(){
+		var priceR = document.getElementsByName('priceR');
+        var quantityR = document.getElementsByName('numR');
+		var tot = 0;
+		
+			quantityR[0].addEventListener("keyup", function() {
+
+            if(priceR[0].value != "" && quantityR[0].value != "")
+			{
+             tot += parseInt(quantityR[0].value) * parseInt(priceR[0].value);
+            document.querySelector("#totalSC").innerHTML = tot;
+			}
+	});	
+}
+
+	static getInstance(){
+		if(!Main._instance){
+			Main._instance = new Main(); 
+			return Main._instance;
+		}
+		else{
+			throw SyntaxError('Cannot create a second Course instance');
+			}
+		}
+}
